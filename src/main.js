2cff9bab1f534b74bf6236b7aca9c3c0e5c52c59
Mjhuu <1441901570@qq.com>
2020-05-11 11:47:50 +0800

    vue可视化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementUI from 'element-ui';
+import './element-variables.scss'
+import moment from 'moment';
+import ViewUI from 'view-design';
+import '../my-theme/index.less';
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles);
+Vue.use(ViewUI);
+import HighchartsVue from 'highcharts-vue'
 
-Vue.config.productionTip = false
+Vue.use(HighchartsVue);
+
+moment.locale('zh-cn');
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
